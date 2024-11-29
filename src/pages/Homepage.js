aa68d6549ae5031460e933a3c558f5706df2f361
ama-matioc <matiocamalia@gmail.com>
2024-11-29 18:47:43 +0200

    added css

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,35 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import '../App.css'
 
 const Homepage = () => {
 
+    const [products, setProducts] =useState([]);
     const handleGetRequest = async () => {
         try {
-            const response = await axios.get('http://localhost:5000/get');
-            console.log(response.data);
+            const response = await axios.get('https://dummyjson.com/product');
+            console.log(response.data.products);
+            setProducts(response.data.products);
         } catch (error) {
             console.error(error);
         }
     };
 
-    const handlePostRequest = async () => {
-        const payload = {
-            key1: 'value1',
-            key2: 'value2',
-        };
-        try {
-            const response = await axios.post('http://localhost:5000/post', payload);
-            console.log(response.data);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+    useEffect(() => {
+        handleGetRequest();
+    }, []);
 
     return (
         <div>
             <h1>Shop Manager</h1>
-            <button onClick={handleGetRequest}>GET Request</button>
-            <button onClick={handlePostRequest}>POST Request</button>
+            <div className="search-add-container">
+    <input 
+        type="text"
+        placeholder="Search for a product"
+        className="search-bar"
+    />
+        <button className="add-btn">Add Product</button>
+    </div>
+
+            <div className='product-container'>
+                {   products.map(product => (
+                        <div key={product.id} className='product-card'>
+                            <img src={product.thumbnail} alt="image"/>
+                            <h3>{product.title}</h3>
+                            <p>{product.description}</p>
+                            <p>{product.price}</p>
+                        </div>
+                    ))
+                }
+            </div>
+
         </div>
     );
 };
