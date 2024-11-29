683e485bbe47acd23b7094d557c8ae0ffd7feb85
arianaradu <arianahraduu@gmail.com>
2024-11-29 21:48:58 +0200

    add addproduct page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
+import AddProduct from './pages/AddProduct';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/addProduct" element={<AddProduct />} />
       </Routes>
     </Router>
   );
